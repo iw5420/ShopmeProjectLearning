c499b82ac19fd20ed3db00ecd603b9aa382c8565
Ian <iw5420@gmail.com>
2021-10-10 10:09:31 +0800

    #71 exercise: code update Category function

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/category/CategoryService.java
@@ -0,0 +1,103 @@
+package com.shopme.admin.category;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.Category;
+
+@Service
+public class CategoryService {
+	@Autowired 
+	private CategoryRepository repo;
+
+	public List<Category> listAll() {
+		List<Category> rootCategories = repo.findRootCategories();
+		return listHierarchicalCategories(rootCategories);
+	}
+	
+	private List<Category>listHierarchicalCategories(List<Category>rootCategories){
+		List<Category> hierarchicalCategories = new ArrayList<>();
+		
+		for(Category rootCategory : rootCategories) {
+			hierarchicalCategories.add(Category.copyFull(rootCategory));
+			
+			Set<Category>children = rootCategory.getChildren();
+			
+			for(Category subCategory : children) {
+				String name = "--" + subCategory.getName();
+				hierarchicalCategories.add(Category.copyFull(subCategory, name));
+				listSubHierarchicalCategories(hierarchicalCategories, subCategory, 1);
+			}
+		}
+		return hierarchicalCategories;		
+	}
+	
+	private void listSubHierarchicalCategories(List<Category> hierarchicalCategories,
+			Category parent, int subLevel) {
+		Set<Category>children = parent.getChildren();
+		int newSubLevel = subLevel + 1;
+		for(Category subCategory : children) {
+			String name = "";
+			for(int i = 0 ; i < newSubLevel ; i++) {
+				name += "--";
+			}
+			name += subCategory.getName();
+			
+			hierarchicalCategories.add(Category.copyFull(subCategory, name));
+			
+			listSubHierarchicalCategories( hierarchicalCategories, subCategory, newSubLevel);
+		}
+	}
+	
+	
+	public Category save(Category category) {
+		return repo.save(category);
+	}
+	
+	public List<Category> listCategoriesUsedInForm(){
+		List<Category>categoriesUsedInForm = new ArrayList<>();
+		Iterable<Category> categories = repo.findAll();
+		for(Category category : categories) {
+			if(category.getParent() == null) {
+				categoriesUsedInForm.add(Category.copyIdAndName(category));
+				Set<Category>children = category.getChildren();
+				
+				for(Category subCategory : children) {
+					String name = "--" +subCategory.getName();
+					categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
+					listSubCategoriesUsedInform(categoriesUsedInForm, subCategory, 1);
+				}
+			}	
+		}
+		return categoriesUsedInForm;
+	}
+	
+	private void listSubCategoriesUsedInform(List<Category>categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category>children = parent.getChildren();
+		
+		for(Category subCategory : children) {
+			String name = "";
+			for(int i = 0; i<newSubLevel; i++) {
+				name += "--";
+				
+			}
+			name+=subCategory.getName();
+			categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
+			listSubCategoriesUsedInform(categoriesUsedInForm, subCategory, newSubLevel);
+		}
+	}
+	
+	public Category get(Integer id) throws CategoryNotFoundException {
+		try {
+			return repo.findById(id).get();
+		} catch (Exception e) {
+			throw new CategoryNotFoundException("Could not find any category with ID "+id);
+		}
+	}
+
+}
